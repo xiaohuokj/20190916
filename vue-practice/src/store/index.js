fafdb3330fcebc75570a9fbf7677d550e3cee16f
wwww <18366707071@163.com>
2019-09-17 18:08:23 +0800

    。。

--- vue-practice/src/store/index.js
+++ vue-practice/src/store/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  plugins: [createPersistedState({
+    storage: window.sessionStorage
+  })],
   state: {
     message: '',
     aTodos: [{id: 0, value: 'default'}]
